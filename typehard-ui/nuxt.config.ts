250bec455f644389345f85a8f3a5f72d6ad5fe77
Sahil Jaura <sjaura@thesummitgrp.com>
2025-04-30 15:11:53 +0530

    nuxt-setup

---
+++ typehard-ui/nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/eslint',
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@pinia/nuxt'
+  ],
+  css: ['~/assets/css/main.css']
+})
